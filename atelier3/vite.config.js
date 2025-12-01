c0bf622c333a29fc098fc6665aa77d91bdeff1c3
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2025-12-01 17:02:00 +0100

    Fix again

--- atelier3/vite.config.js
+++ atelier3/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FrontEndDevelopment/atelier3/'   // IMPORTANT
+  base: '/react-ateliers/atelier3/'   // IMPORTANT
 })
